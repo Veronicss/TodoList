d852a57e6a94aaed7b5d873d478681aa0ba06e47
Veronicss <splashveronicac4@gmail.com>
2023-03-21 14:39:42 +0800

    File

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValue.trim()) {
+      return;
+    }
+    setTodos([...todos, { text: inputValue, completed: false }]);
+    setInputValue('');
+  };
+
+  const handleTodoDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  
+  const handleTodoComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+         <li key={index} className={todo.completed ? 'completed' : ''} style={{ textAlign: 'left' }}>           
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => handleTodoComplete(index)}
+            />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </span>
+            <button onClick={() => handleTodoDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
